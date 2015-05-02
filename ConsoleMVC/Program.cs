90d96783f39252f26612aebdff1b9a1a912448d8
Shiva Sharma <shivathebravo@gmail.com>
2015-05-02 10:34:37 -0400

    Initial Commit

---
+++ ConsoleMVC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleMVC
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+       
+    }
+
+    public class Album
+    {
+        public int AlbumId { get; set; }
+        public string  Title { get; set; }
+        public decimal Price { get; set; }
+    }
+
+    public class MusicContext : DbContext
+    {
+        public DbSet<Album> Albums { get; set; }
+    }
+
+}
